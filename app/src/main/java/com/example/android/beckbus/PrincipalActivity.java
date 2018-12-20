235818e1465673fe155b9c3a62c8d2b6f08f7e89
KICHNA Abdellatif <akichna@gmail.com>
2018-12-20 17:58:35 +0100

    First change

--- app/src/main/java/com/example/android/beckbus/PrincipalActivity.java
+++ app/src/main/java/com/example/android/beckbus/PrincipalActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.beckbus;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
